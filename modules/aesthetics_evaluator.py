9d9881a416c421886916e0966d4db33d003af1c6
Mujo Agents <maitran080842@gmail.com>
2024-12-14 14:58:23 -0700

    Create aesthetics_evaluator.py

---
+++ modules/aesthetics_evaluator.py
@@ -0,0 +1,4 @@
+class AestheticsEvaluator:
+    def evaluate(self, styles):
+        print("Evaluating the aesthetic quality of the generated agent...")
+        return True  # Always approve for simplicity
