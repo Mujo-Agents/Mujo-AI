90d6f3d1f52fc9dc911dfda42e4c9755e771bfea
Mujo Agents <maitran080842@gmail.com>
2024-12-14 14:59:01 -0700

    Create file_handler.py

---
+++ utils/file_handler.py
@@ -0,0 +1,12 @@
+import json
+
+class FileHandler:
+    @staticmethod
+    def load_json(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+
+    @staticmethod
+    def save_json(file_path, data):
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
